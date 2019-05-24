0185dfe9ede820083b1bee75f892e3a1e06d3a19
svanessen <shannon.vanessen@hotmail.com>
2019-05-23 20:37:29 -0500

    Adding base source code

---
+++ .sfdx/typings/lwc/contentassets.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/contentAssetUrl/invest_logo4" {
+    var invest_logo4: string;
+    export default invest_logo4;
+}
---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/resourceUrl/DataCache" {
+    var DataCache: string;
+    export default DataCache;
+}
+declare module "@salesforce/resourceUrl/nouislider" {
+    var nouislider: string;
+    export default nouislider;
+}
